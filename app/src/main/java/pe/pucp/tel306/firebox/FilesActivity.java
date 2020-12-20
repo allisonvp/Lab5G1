e852c6e2635a338c8fe2d8f7513be83c85e6e059
katty <a20160555@pucp.edu.pe>
2020-12-20 10:09:36 +0530

    login

--- app/src/main/java/pe/pucp/tel306/firebox/FilesActivity.java
+++ app/src/main/java/pe/pucp/tel306/firebox/FilesActivity.java
@@ -2,7 +2,15 @@ package pe.pucp.tel306.firebox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FilesActivity extends AppCompatActivity {
 
@@ -10,5 +18,29 @@ public class FilesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files);
+
+        setTitle("Profile");
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            String displayName = currentUser.getDisplayName();
+            String email = currentUser.getEmail();
+
+            TextView textViewBienvenido = findViewById(R.id.textViewBienvenido);
+            textViewBienvenido.setText("Bienvenido " + displayName);
+        }
+
+    }
+
+    public void logout (View view) {
+        AuthUI instance = AuthUI.getInstance();
+        instance.signOut(this).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(FilesActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
+++ app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
@@ -1,8 +1,24 @@
 package pe.pucp.tel306.firebox;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("FireBox");
+
+        validacionUsuario();
+    }
+
+    public void login (View view) {
+        List<AuthUI.IdpConfig> proveedores = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        AuthUI instance = AuthUI.getInstance();
+        Intent intent = instance
+                .createSignInIntentBuilder()
+                .setAvailableProviders(proveedores)
+                .build();
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == 1) {
+            validacionUsuario();
+        }
+    }
+
+    public void validacionUsuario() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            currentUser.reload();
+            if (currentUser.isEmailVerified()) {
+                startActivity(new Intent(this, FilesActivity.class));
+                finish();
+            } else {
+                Toast.makeText(MainActivity.this, "Se le envió un correo para verificar su cuenta", Toast.LENGTH_SHORT).show();
+                currentUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("infoApp", "Correo Enviado");
+                    }
+                });
+
+            }
+        }
     }
 }
